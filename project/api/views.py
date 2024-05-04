1b29fa5a04b8ddaaee900244b4eb4ca22e8bdbb1
Shreya <shreyadwivedi477@gmail.com>
2024-05-04 16:25:06 +0530

    first commit

---
+++ project/api/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly,AllowAny
+
+class MovieViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAdminUser]
+	queryset = MovieModel.objects.all()
+	serializer_class = MovieSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAdminUser]
+	queryset = StudentModel.objects.all()
+	serializer_class = StudentSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
